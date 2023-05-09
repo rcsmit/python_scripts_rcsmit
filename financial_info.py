89962a1fa804a96ef63995c9883a529c7643b122
Rene Smit <rcx.smit@gmail.com>
2023-05-09 21:34:27 +0200

    .

--- financial_info.py
+++ financial_info.py
@@ -6,10 +6,24 @@ from datetime import datetime, timedelta
 import traceback
 from collections import defaultdict
 
+"""Script to manage a double entry accounting system. Made with the help of Chat-GPT
 
+In a double-entry accounting system, every financial transaction is recorded in at least 
+two different accounts: one account is credited and the other account is debited. The debited account 
+is the one that receives the payment, and the credited account is the one that makes the payment.
+
+"""
 def read():
-    file = r"C:\Users\rcxsm\Documents\xls\masterfinance_2023_dummy.xlsm"
+    """Read the data. A dummy file can be found at 
+     https://github.com/rcsmit/python_scripts_rcsmit/blob/master/input/masterfinance_2023_dummy.xlsm
+     Attention : de "KRUIS"-accounts are not 0 because the numbers are randomized
+
+    Returns:
+        df : The dataframe with the data
+    """    
+    file = r"C:\Users\rcxsm\Documents\xls\masterfinance_2023.xlsm"
     file = r"C:\Users\rcxsm\Documents\python_scripts\python_scripts_rcsmit\input\masterfinance_2023_dummy.xlsm"
+    # to be found on
     try:
         df = pd.read_excel (file,
                             sheet_name= "INVOER",
@@ -34,6 +48,8 @@ def read():
     return df
 
 class Account:
+    """Handle the various accounts
+    """    
     def __init__(self, name):
         self.name = name
         self.balance = 0
@@ -53,6 +69,8 @@ class Account:
 
 
 class Transaction:
+    """Handle the transactions
+    """    
     def __init__(self, date, debit_account, credit_account, amount, description):
         self.date = date
         self.debit_account = debit_account
@@ -61,6 +79,8 @@ class Transaction:
         self.description = description
 
 class Ledger:
+    """Handle the ledgers (=grootboeken)
+    """    
     def __init__(self):
         self.accounts = []
         self.transactions = []
@@ -81,6 +101,8 @@ class Ledger:
         self.transactions.append(transaction)
         
     def print_trial_balance(self):
+        """Make the trial balance over all transactions
+        """        
         total_debits = 0
         total_credits = 0
         for account in self.accounts:
@@ -91,6 +113,16 @@ class Ledger:
         print(f"Total Credits: {total_credits}")
 
     def ask_trial_balance_date(self, asked_acount, date_given):
+        """Return the value on the trial balance of a certain account on a certain date. 
+        Used to make a monthly pivot table
+
+        Args:
+            asked_acount (str): the asked account
+            date_given (date): date
+
+        Returns:
+            float: the amount on the trial balance
+        """
         total_debits = 0
         total_credits = 0
         to_return = 0
@@ -105,27 +137,39 @@ class Ledger:
             total_credits += c
             if account.name == asked_acount:
                 to_return = d+c
-            # print (f"{account.name} - {d} {c}")
-            #print(account)
-        # print(f"Total Debits: {total_debits}")
-        # print(f"Total Credits: {total_credits}")
+            
         return to_return
 
 
 def get_monthly_totals(ledger):
+    """function to calculate the monthly totals
+
+    Args:
+        ledger (_type_): _description_
+
+    Returns:
+        dictionary: dictonary with the monthly totals
+    """    
     monthly_totals = defaultdict(lambda: defaultdict(float))
     for transaction in ledger.transactions:
         year, month, day = transaction.date.year, transaction.date.month, transaction.date.day
         account_name = transaction.credit_account.name if transaction.credit_account.name != "None" else transaction.debit_account.name
         monthly_totals[account_name][f"{year}-{month:02d}"] += transaction.amount
-    print  ("monthly_totals")
-    print  (monthly_totals)
+
     
     return monthly_totals
 
 
 
 def get_monthly_pivot_table(ledger):
+    """make the montly totals
+
+    Args:
+        ledger (_type_): _description_
+
+    Returns:
+        df: dataframe with the montly totals
+    """    
     monthly_totals = get_monthly_totals(ledger)
     df = pd.DataFrame(monthly_totals)
     df = df.fillna(0)  # replace NaN values with 0
@@ -137,6 +181,12 @@ def get_monthly_pivot_table(ledger):
 
 
 def print_pivot_monthlty_trial_balance(ledger, accounts_source):
+    """Make a pivot table with a montly trial balance
+
+    Args:
+        ledger (_type_): _description_
+        accounts_source (_type_): list with the sources (cash, bank accounts etc)
+    """    
     # Define the start and end year
     start_year = 2023
     end_year = 2023
@@ -160,20 +210,33 @@ def print_pivot_monthlty_trial_balance(ledger, accounts_source):
     pivot_table = pd.pivot_table(trial_balance_df, values='Balance', index=['Account'], columns=['Date'])
 
     # Print the pivot table
-    print(pivot_table)
+    return pivot_table
 
 
 def main():
-    df = (read())
-    
-    ledger = Ledger()
     modus = "main_category"
     modus = "income_expenses"
-    # create some accounts
-
+    
+    df = (read())
     accounts = pd.concat([df['source'], df[modus]]).unique().tolist()
     accounts_source = (df['source']).unique().tolist()
 
+    ledger = create_ledger(df, accounts, modus)
+   
+    print("monthly_pivot_table")
+    print(get_monthly_pivot_table(ledger))
+
+    # print trial balance
+    print("ledger.print_trial_balance()")
+    ledger.print_trial_balance()
+
+    # print trial balance on a certain date
+    print()
+    print("print trial balance on a certain date")
+    print(print_pivot_monthlty_trial_balance(ledger, accounts_source))
+
+def create_ledger(df, accounts, modus):
+    ledger = Ledger()
     
     for a in accounts:
         a_ = Account(a)
@@ -189,22 +252,7 @@ def main():
             row["bedrag"],
             row["description"]
         )
-
-   
-
-    monthly_pivot_table = get_monthly_pivot_table(ledger)
-    print("monthly_pivot_table")
-
-    print(monthly_pivot_table)
-
-    # print trial balance
-    print("ledger.print_trial_balance()")
-    ledger.print_trial_balance()
-
-    # print trial balance on a certain date
-    print()
-    print("print trial balance on a certain date")
-    
-    print_pivot_monthlty_trial_balance(ledger, accounts_source)
+        
+    return ledger
 
 main()
